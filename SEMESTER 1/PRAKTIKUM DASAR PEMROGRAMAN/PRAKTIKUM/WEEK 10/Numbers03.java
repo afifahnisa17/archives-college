3e46a6a62e6933d54d6af2ba99615310dee2a38d
afifahnisa17 <afifahnisa041@gmail.com>
2023-10-30 16:35:39 +0700

    Modifikasi percobaan 3

--- SEMESTER 1/PRAKTIKUM DASAR PEMROGRAMAN/PRAKTIKUM/WEEK 10/Numbers03.java
+++ SEMESTER 1/PRAKTIKUM DASAR PEMROGRAMAN/PRAKTIKUM/WEEK 10/Numbers03.java
@@ -1,9 +1,19 @@
+import java.util.Arrays;
 public class Numbers03 {
     public static void main(String[] args) {
-        int[][] myNumbers = new int[3][];
-        myNumbers[0] = new int[5];
-        myNumbers[1] = new int[3];
-        myNumbers[2] = new int[1];
+        int[][] myNumbers = new int[3][5]; //Panjang Array diatur sama yaitu 5 untuk setiap baris.
+        // int[][] myNumbers = new int[3][];
+        // myNumbers[0] = new int[5];
+        // myNumbers[1] = new int[3];
+        // myNumbers[2] = new int[1];
+
+        for (int i = 0; i<myNumbers.length; i++) {
+            System.out.println(Arrays.toString(myNumbers[i]));
+        }
+
+        for(int i = 0; i < myNumbers.length; i++) {
+            System.out.println("Panjang bari ke-" + (i+1) + ": " + myNumbers[i]. length);
+        }
     }
     
 }
