099a5d3752fea29ab9077df437d5dddb87ce14e1
afifahnisa17 <afifahnisa041@gmail.com>
2023-10-30 13:48:20 +0700

    Menambahkan commit

--- SEMESTER 1/PRAKTIKUM DASAR PEMROGRAMAN/PRAKTIKUM/WEEK 10/Bioskop03.java
+++ SEMESTER 1/PRAKTIKUM DASAR PEMROGRAMAN/PRAKTIKUM/WEEK 10/Bioskop03.java
@@ -10,14 +10,22 @@ public class Bioskop03 {
         penonton [3] [0] = "Gisel";
         penonton [3] [1] = "Hana";
 
-        System.out.printf("%s \t %s \n", penonton[0][0], penonton[0][1]);
-        System.out.printf("%s \t %s \n", penonton[1][0], penonton[1][1]);
-        System.out.printf("%s \t %s \n", penonton[2][0], penonton[2][1]);
-        System.out.printf("%s \t %s \n", penonton[3][0], penonton[3][1]);
+        System.out.println("Penonton pada baris ke-3: ");
 
-        System.out.println(penonton.length);
-        for (int i =0; i < penonton.length; i++) {
-            System.out.println("Panjang baris ke-" + (i+1) + ": " + penonton[i].length);
+        for (int i = 0; i < penonton [2].length; i++) {
+            System.out.println(penonton[2][i]);
         }
+        
+        // System.out.printf("%s \t %s \n", penonton[0][0], penonton[0][1]);
+        // System.out.printf("%s \t %s \n", penonton[1][0], penonton[1][1]);
+        // System.out.printf("%s \t %s \n", penonton[2][0], penonton[2][1]);
+        // System.out.printf("%s \t %s \n", penonton[3][0], penonton[3][1]);
+
+        // System.out.println(penonton.length);
+        // for (int i =0; i < penonton.length; i++) {
+        //     System.out.println("Panjang baris ke-" + (i+1) + ": " + penonton[i].length);
+        // }
+        
+        
     }
 }
