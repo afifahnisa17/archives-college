545915ab1c8ad69c7e2b00ddffc4408f5f3320c2
afifahnisa17 <afifahnisa041@gmail.com>
2023-11-06 15:19:37 +0700

    Jobsheet 11

---
+++ SEMESTER 1/PRAKTIKUM DASAR PEMROGRAMAN/PRAKTIKUM/WEEK 11/NestedLoop_03.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class NestedLoop_03 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double temps[][] = new double[5][7];
+
+        for (int i = 0; i < temps.length; i++) {
+            System.out.println("Kota ke-" + i);
+            double totalSuhu = 0; 
+            for (int j = 0; j < temps[0].length; j++) {
+                System.out.print("Hari ke-" + (j + 1) + ": ");
+                temps[i][j] = input.nextDouble();
+                totalSuhu += temps[i][j]; 
+            }
+            System.out.println();
+            double rataRataKota = totalSuhu / temps[0].length; 
+            System.out.println("Rata-rata suhu kota ke-" + i + ": " + rataRataKota);
+        }
+        
+        for (int i = 0; i < temps.length; i++) {
+            System.out.println("Kota ke-" + (i + 1) + ": ");
+            for (int j = 0; j < temps[0].length; j++) {
+                System.out.print(temps[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
--- SEMESTER 1/PRAKTIKUM DASAR PEMROGRAMAN/PRAKTIKUM/WEEK 11/Square.java
+++ SEMESTER 1/PRAKTIKUM DASAR PEMROGRAMAN/PRAKTIKUM/WEEK 11/Square.java
@@ -1,14 +1,18 @@
 import java.util.Scanner;
 
-public class Star {
+public class Square {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Masukkan nilai N = ");
-        int N =sc.nextInt();
+        int N = sc.nextInt();
 
-        for (int i = 0; i  <= N; i--) {      
+        for (int iOuter=1; iOuter<=N; iOuter++) {
+            for (int i = 0; i<=N; i++) {
             System.out.print("*");
+            }
+        System.out.println();
         }
-    }
+    } 
 }
+
 
