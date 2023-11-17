582ece8f8f3325a26284816aab9539c129ce00be
Afifah Khoirunnisa <afifahnisa041@gmail.com>
2023-11-17 23:13:52 +0700

    menambahkan baris baruuu

--- SEMESTER 1/PRAKTIKUM DASAR PEMROGRAMAN/PRAKTIKUM/WEEK 1/MyFirstJava03.java
+++ SEMESTER 1/PRAKTIKUM DASAR PEMROGRAMAN/PRAKTIKUM/WEEK 1/MyFirstJava03.java
@@ -2,7 +2,7 @@ public class MyFirstJava03 {
     public static void main(String[] args) {
         System.out.println("Nama saya Afifah Khoirunnisa");
         System.out.println("Saya adalah mahasiswa baru Jurusan Teknologi Informasi-POLINEMA");
-        System.out.println("Saya mahasiswa semester 1");
+        System.out.println("Saya saat ini sedang belajar java");
         
 
     }
